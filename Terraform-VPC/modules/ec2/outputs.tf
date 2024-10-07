19f7de1b586e7c374fc445a726fd8efd05804c7a
meowwkhoa <22520665@gm.uit.edu.vn>
2024-10-07 08:49:40 +0700

    Add a minor change

--- Terraform-VPC/modules/ec2/outputs.tf
+++ Terraform-VPC/modules/ec2/outputs.tf
@@ -1,19 +1,19 @@
 output "public_instance_id" {
   description = "The ID of the public EC2 instance"
-  value       = aws_instance.public_ec2.id
+  value       = aws_instance.public.id
 }
 
 output "private_instance_id" {
   description = "The ID of the private EC2 instance"
-  value       = aws_instance.private_ec2.id
+  value       = aws_instance.private.id
 }
 
 output "public_instance_public_ip" {
   description = "The public IP address of the public EC2 instance"
-  value       = aws_instance.public_ec2.public_ip
+  value       = aws_instance.public.public_ip
 }
 
 output "private_instance_private_ip" {
   description = "The private IP address of the private EC2 instance"
-  value       = aws_instance.private_ec2.private_ip
+  value       = aws_instance.private.private_ip
 }
