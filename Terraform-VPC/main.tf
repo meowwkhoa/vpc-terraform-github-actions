db7de3409584aaa15cdea22657b1f0696449388e
meowwkhoa <22520665@gm.uit.edu.vn>
2024-10-07 20:41:57 +0700

    Add a minor change

--- Terraform-VPC/main.tf
+++ Terraform-VPC/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-1"
 }
 
 
@@ -8,7 +8,7 @@ module "vpc" {
   vpc_cidr            = "10.0.0.0/16"
   public_subnet_cidr  = "10.0.1.0/24"
   private_subnet_cidr = "10.0.2.0/24"
-  az                  = "us-east-1a"
+  az                  = "us-west-1a"
 }
 
 module "security_groups" {
