00ff7833ea0ae3c7a1253cdf184ab0c7c633067e
meowwkhoa <22520665@gm.uit.edu.vn>
2024-10-07 19:56:19 +0700

    Add a minor change

--- Terraform-VPC/main.tf
+++ Terraform-VPC/main.tf
@@ -21,7 +21,7 @@ module "security_groups" {
 
 module "ec2" {
   source                = "./modules/ec2"
-  ami                   = "ami-12345678"
+  ami                   = "ami-0c55b159cbfafe1f0"
   instance_type         = "t2.micro"
   public_subnet_id      = module.vpc.public_subnet_id
   private_subnet_id     = module.vpc.private_subnet_id
