b9c15b088ff0e5b23d8fe24cdb10e5e82d34326c
qkhanh39 <khanhtruong3904@gmail.com>
2024-11-10 20:03:27 +0700

    Change according to checkov

--- Terraform-VPC/modules/EC2/main.tf
+++ Terraform-VPC/modules/EC2/main.tf
@@ -3,18 +3,6 @@ resource "aws_instance" "public" {
   instance_type = var.instance_type
   subnet_id = var.public_subnet_id
   security_groups = [var.public_security_group]
-  ebs_optimized = true
-  monitoring = true
-  iam_instance_profile = "test"
-
-  root_block_device {
-    encrypted = true
-  }
-
-  metadata_options {
-    http_endpoint = "enabled"
-    http_tokens = "required"
-  }
 
   tags = {
     Name = "Public Instance group 12"
@@ -26,19 +14,6 @@ resource "aws_instance" "private" {
   instance_type = var.instance_type
   subnet_id = var.private_subnet_id
   security_groups = [var.private_security_group]
-  ebs_optimized = true
-  monitoring = true
-  iam_instance_profile = "test"
-  
-  root_block_device {
-    encrypted = true
-  }
-
-  metadata_options {
-    http_endpoint = "enabled"
-    http_tokens = "required"
-  }
-
 
   tags = {
     Name = "Private Instance group 12"
